aa40026ec734c1b61d73c5025b72f59ddd72d21a
alaminfaraji <alaminice1617@gmail.com>
2024-05-20 10:06:30 +0600

    Add Remote Database

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,12 +1,15 @@
 const { Pool } = require("pg");
 const { z } = require("zod");
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 const postSchema = z.object({
